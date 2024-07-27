f792c56f926388ad1d00f5f1efd8c9a3be1aa779
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-07-27 17:20:40 +0300

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Khaled Hossameldin on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
